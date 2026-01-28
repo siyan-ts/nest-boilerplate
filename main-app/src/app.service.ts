0fe5e9d5dc2a2ef8982baaca4a6f078057e50990
Ravi Prakash <raviprakash.xpd54@gmail.com>
2026-01-28 16:59:13 +0800

    update use microservice

--- main-app/src/app.service.ts
+++ main-app/src/app.service.ts
@@ -1,23 +1,22 @@
-import { Injectable } from '@nestjs/common';
-import { HttpService } from '@nestjs/axios';
-import { firstValueFrom } from 'rxjs';
+import { Injectable } from "@nestjs/common";
+import { HttpService } from "@nestjs/axios";
+import { firstValueFrom } from "rxjs";
 
 @Injectable()
 export class AppService {
   constructor(private readonly httpService: HttpService) {}
 
   getHello(): string {
-    const envMessage = process.env.FROM_MAIN || 'from main';
+    const envMessage = process.env.FROM_MAIN || "from main";
     return `hello ${envMessage}`;
   }
 
   async getHelloFromMicroservice(): Promise<string> {
     try {
-      const microserviceHost = process.env.MICROSERVICE_HOST || 'localhost';
-      const microservicePort = process.env.MICROSERVICE_PORT || '3001';
-      const microserviceUrl = `http://${microserviceHost}:${microservicePort}`;
+      const microserviceUrl =
+        process.env.MICROSERVICE_URL || "http://localhost:3001"; // Updated to use MICROSERVICE_URL
       const response = await firstValueFrom(
-        this.httpService.get(`${microserviceUrl}/greeting`)
+        this.httpService.get(`${microserviceUrl}/greeting`),
       );
       return response.data;
     } catch (error) {
